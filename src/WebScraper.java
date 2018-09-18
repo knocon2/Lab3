2bb9dc9cf30a8a2841c0666f80718cc1348f6de7
knocon2 <knocon2@illinois.edu>
2018-09-18 14:04:58 -0500

    one word check

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static int countOneWord(final String url, final String word) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return 0;
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+
+        String[] stringArray = contents.split(" ");
+        int count = 0;
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].equalsIgnoreCase(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        System.out.println(countOneWord("http://erdani.com/tdpl/hamlet.txt", "the"));
     }
 }
